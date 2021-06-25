908e12b233adcdef2e25f74b81c086f87812a1c2
paulajlippe <71293269+paulajlippe@users.noreply.github.com>
2021-06-25 16:21:51 -0500

    update

--- js/app.js
+++ js/app.js
@@ -15,48 +15,41 @@
 
 /** Define Global Variables **/
 
-const section = document.getElementById('section');
+const sections = document.getElementsByTagName('section');
 const nav = document.getElementsByTagName('navbar__menu');
-const navLinks = document.getElementById('navbar__list');
 
 /** Begin Main Functions **/
 
-
 // BUILD THE NAVBAR
-//Navigation is built dynamically as an unordered list.
-//Make sure the navbar be built dynamically using loops & DOM manipulation techniques like `createElement()` & `appendChild()`
-
-// 1 - Fetch the list of all sections
-// 2 - Fetch the .nav-list unordered list
-// 3 - Run a loop over list of sections
-// i. Generate a new <li> for each section
-// ii. Insert it inside the .nav-list
+// Navigation is built dynamically as an unordered list.
+// Make sure the navbar be built dynamically using loops & DOM manipulation techniques like `createElement()` & `appendChild()`
 
 var ul = document.getElementById('navbar__list');
 
 function createNav() {
-    for(let i = 0.; i < allSection.length; i ++){
+    for(let i = 0; i < sections.length; i ++){
         const navLi = document.createElement('li');
         const navLiLink = document.createElement ('a');
-        navLi.innerText = 'section id' + i;
-            navLi.appendChild(navLiLinks);
+        navLiLink.textContent = sections[i].id;
+        navLiLink.classList.add("menu__link");
+        navLi.appendChild(navLiLink);
+        ul.appendChild(navLi)
             }
         }
+createNav()
 
 // looping through all the sections
 // create a new list element each run through the loop called 'tabs'
 // set the innerHTML of that element equal to the data attribute of the sections HTMLCollection. Double check the value there! You just want the individual item not the whole sections collection
 // append the li 'tabs' to the nav
 
-
-
 // Add class 'active' to section when near top of viewport
 
 function sectionActive() {
+    const active = sectionActive;
     const scrollHeight = window.pageYOffset;
 
-    for(let i = 1; i <= section.length; i++) {
-        const tab = document.createElement('li');
+    for(let i = 0; i <= sectionActive.length; i++) {
         tabs.innerHTML = sections[i].getAttribute('data-nav');
         nav.appendChild(tab);
         const sectionTop = section.getBoundingClientRect().top;
@@ -64,11 +57,49 @@ function sectionActive() {
     }
 };
 
-window.addEventListener('scroll', function(){
+window.addEventListener('click', function(){
     sectionActive();
 });
 
+// function setActive(){
+//     for(navLiLink of sections){
+//         if(navchecksize(navLiLink)){
+//             // use the same technique to 
+//             // add the class to the navbar item
+//             navLiLink.classList.add("active__class");
+//         }else{        
+//             navLiLink.classList.remove("active__class");
+//         }
+//     }
+// };
+
+
 // Scroll to anchor ID using scrollTO event
+function scrollToAnchor(event) {
+    event.preventDefault();
+  
+    let section = document.querySelector(`#${event.target.dataset.section_id}`);
+    window.scrollTo({
+        top: section.offsetTop, // using section's left and top coordinated
+        left: section.offsetLeft,
+        behavior: 'smooth'
+    });
+}
+
+
+
+
+  /* HIGHLIGHTING NAV IN VIEW */
+
+// Scroll to anchor ID using scrollTO event 
+
+// function scroll_to_anchor(section_id){
+//     var tag = $("#"+section_id);
+//     $('html,body').animate({scrollTop: tag.offset().top},'smooth');
+// };
+
+
+
 
 /** End Main Functions * Begin Events **/
 
@@ -76,8 +107,8 @@ window.addEventListener('scroll', function(){
 // Scroll to section on link click
 // Set sections as active
 
-// Make sections colaspable
-const col = document.getElementsByClassName("collapsible");
+// Make sections colaspable - WIP
+const col = document.getElementsByTagName("collapsible");
 var i;
 for (i = 0; i < col.length; i++) {
     col[i].addEventListener("click", function() {
@@ -90,3 +121,25 @@ for (i = 0; i < col.length; i++) {
     }
     });
     }
+
+// Mobile Burger Nav - WIP
+const navbarSlide = () => {
+const burger = document.querySelector('.burger');
+// const navbarLinks = document.querySelectorAll('.navbar__menu li');
+const navbarLinks = document.querySelectorAll('.navbar__list');
+
+
+burger.addEventListener('click', () => {
+    //toggle
+    nav.classList.toggle('navbar__menu active');
+    //animate
+    navbar__list.forEach((link, index) => {
+    if (link.style.animation) {
+        link.style.animation = ""
+    } else {
+        link.style.animation = 'navbar__listFade 0.7s ease forwards ${index/7}s';
+    }
+    });
+});
+}
+navbarSlide();
