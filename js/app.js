414d72ec973f69532b64792f96f970c654e7983a
paulajlippe <71293269+paulajlippe@users.noreply.github.com>
2021-07-07 19:31:17 -0500

    Nav anchors working

--- js/app.js
+++ js/app.js
@@ -1,143 +1,156 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
-/** Define Global Variables **/
+document.addEventListener('DOMContentLoaded', () => {
 
-const sections = document.getElementsByTagName('section');
-const nav = document.getElementsByTagName('navbar__menu');
+/**  Define Global Variables */
 
-/** Begin Main Functions **/
+const sectionsList = document.querySelectorAll('section');
+const navbarParent = document.querySelector('#navbar__list');
 
-// BUILD THE NAVBAR
-// Navigation is built dynamically as an unordered list.
-// Make sure the navbar be built dynamically using loops & DOM manipulation techniques like `createElement()` & `appendChild()`
 
-var ul = document.getElementById('navbar__list');
+/** Helper Functions */
 
-function createNav() {
-    for(let i = 0; i < sections.length; i ++){
-        const navLi = document.createElement('li');
-        const navLiLink = document.createElement ('a');
-        navLiLink.textContent = sections[i].id;
-        navLiLink.classList.add("menu__link");
-        navLi.appendChild(navLiLink);
-        ul.appendChild(navLi)
+    function getCorrespondingMenu(section) {
+        const menuLinksList = document.querySelectorAll('.menu__link');
+        for (const menu of menuLinksList) {
+            if (menu.innerText == section.getAttribute('data-nav')) {
+                return menu;
+            }
         }
-            var scroll = document.getElementById("sections");
-            scroll.addEventListener('click', navLiLink => {
-                navLiLink.preventDefault();
-                sections.scrollIntoView({ behavior: "smooth" })
-                });  
-
-    }; 
-           
-createNav()
-
-
-// looping through all the sections
-// create a new list element each run through the loop called 'tabs'
-// set the innerHTML of that element equal to the data attribute of the sections HTMLCollection. Double check the value there! You just want the individual item not the whole sections collection
-// append the li 'tabs' to the nav
-
-    const tabLinks = new Array();
-    const contentDivs = new Array();
-
-    function init() {
-        // Grab the tab links and content divs from the page
-        var tabListItems = document.getElementById('tabs').childNodes;
-        for ( var i = 0; i < tabListItems.length; i++ ) {
-          if (tabListItems[i].nodeName == "li" ) {
-            var tabLink = getFirstChildWithTagName(tabListItems[i], 'a');
-            var id = getHash( tabLink.getAttribute('href') );
-            tabLinks[id] = tabLink;
-            contentDivs[id] = document.getElementById(id);
-          }
-        }
+        return undefined;
     }
 
 
-// Add class 'active' to section when near top of viewport
+	function isInViewport(elem) {
+		let bounding = elem.getBoundingClientRect();
+		return (
+			bounding.top +125 >=0 &&
+			bounding.left +125 >= 0 &&
+			bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+			bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+			);
+	}
 
-function setActive(){
-    for(tabLink of contentDivs){
-        if(navchecksize(tabLink)){
-            // use the same technique to 
-            // add the class to the navbar item
-            tabLink.classList.add("active__class");
-        }else{        
-            tabLink.classList.remove("active__class");
-        }
+
+/**  Populate Nav Bar */
+
+	function addNavItems() {
+		const navFragment = document.createDocumentFragment();
+		sectionsList.forEach((sectElement) => {
+			const navListItem = document.createElement('li');
+			navListItem.innerText = sectElement.getAttribute('data-nav');
+			navListItem.classList.add('menu__link');
+			navListItem.addEventListener('click', () => {
+				sectElement.scrollIntoView({behavior: "smooth"});
+			});
+			navFragment.appendChild(navListItem);
+		});
+		navbarParent.appendChild(navFragment);
+	}
+
+
+/** Create Scroll To Top Button */
+
+	function createToTopButton() {
+		const toTopButton = document.createElement('button');
+		toTopButton.id = 'toTop';
+		toTopButton.title = 'Go to top of the webpage';
+        toTopButton.classList.add('toTop-button');
+	}
+
+
+/** Events */
+/** Scroll Active State Function */
+
+	function activeSection() {
+		window.addEventListener('scroll', () => {
+			sectionsList.forEach((section) => {
+				const menuItem = getCorrespondingMenu(section);
+                    if (isInViewport(section)) {
+                        menuItem.classList.add('menu__link--active');
+                        section.classList.add('section--active');
+                    }
+                    else {
+                        menuItem.classList.remove('menu__link--active');
+                        section.classList.remove('section--active');
+                    }
+			});
+		});
+	}
+
+
+/** To Top Function */
+
+	function displayToTopButton() {
+		const toTopBtn = document.getElementById('toTop');
+		window.addEventListener('scroll', () => {
+			if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+				toTopBtn.style.display = "block";
+			} 
+            else {
+				toTopBtn.style.display = "none";
+			}
+		});
+	}
+
+
+	function clickToTopButton() {
+		const toTopBtn = document.getElementById('toTop');
+		toTopBtn.addEventListener('click', () => {
+			document.body.scrollTop = 0; 
+			document.documentElement.scrollTop = 0; 
+		});
+	}
+
+
+// Toogle Burger Nav 
+    function navbarToogle () {
+        const burger = document.querySelector('.burger');    
+        burger.addEventListener('click', () => {
+            //toggle
+            navbarParent.classList.toggle('navbar__menu--active');
+            //animate
+            navbar__list.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = 'navbar__listFade 0.7s ease forwards ${index/7}s';
+            }
+            });
+        });
     }
-};
 
+/** Function Calls */
 
-// // Go to Top Function - WIP
-// function createToTopButton() {
-//     //create new buttom element
-//     const topButton = document.createElement('button');
+	// Build menu
+	addNavItems();
 
-//     // add attributes to the button
-//     topButton.id = 'toTopBtn';
-//     topButton.title = 'Go to top of webpage';
-//     topButton.innerText = 'Top';
-//     // add classes
-//     topButton.classList.add('btn');
-//     topButton.classList.add('btn-scrollTop');
+	// To top button
+	createToTopButton();
 
-//     //add button to the webpage
-//     document.body.appendChild(topButton);
-// }
+	// Display to top button
+	displayToTopButton();
 
+	// Click event toTopButton
+	clickToTopButton();
 
-/** End Main Functions * Begin Events **/
+	// Sections as active
+	activeSection();
 
+    // Nav slide for burger menu
+    navbarToogle();
 
-
-// Make sections colaspable - WIP
-// const col = document.getElementsByTagName("collapsible");
-// var i;
-// for (i = 0; i < col.length; i++) {
-//     col[i].addEventListener("click", function() {
-//     this.classList.toggle("sectionActive");
-//     var content = this.nextElementSibling;
-//     if (content.style.display === "block") {
-//     content.style.display = "none";
-//     } else {
-//     content.style.display = "block";
-//     }
-//     });
-//     }
-
-// Mobile Burger Nav - WIP
-const navbarSlide = () => {
-const burger = document.querySelector('.burger');
-// const navbarLinks = document.querySelectorAll('.navbar__menu li');
-const navbarLinks = document.querySelectorAll('.navbar__list');
-
-
-burger.addEventListener('click', () => {
-    //toggle
-    nav.classList.toggle('navbar__menu active');
-    //animate
-    navbar__list.forEach((link, index) => {
-    if (link.style.animation) {
-        link.style.animation = ""
-    } else {
-        link.style.animation = 'navbar__listFade 0.7s ease forwards ${index/7}s';
-    }
-    });
 });
-}
-navbarSlide();
---
+++ landingPageOld.js
@@ -0,0 +1,141 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/** Define Global Variables **/
+
+const sections = document.getElementsByTagName('section');
+const nav = document.getElementsByTagName('navbar__menu');
+
+/** Begin Main Functions **/
+
+// BUILD THE NAVBAR
+// Navigation is built dynamically as an unordered list.
+// Make sure the navbar be built dynamically using loops & DOM manipulation techniques like `createElement()` & `appendChild()`
+
+var ul = document.getElementById('navbar__list');
+
+function createNav() {
+    for(let i = 0; i < sections.length; i ++){
+        const navLi = document.createElement('li');
+        const navLiLink = document.createElement ('a');
+        navLiLink.textContent = sections[i].id;
+        navLiLink.classList.add("menu__link");
+        navLi.appendChild(navLiLink);
+        ul.appendChild(navLi)
+        }
+    }
+        function scroll() {
+            window.addEventListener('click', e => {
+            e.preventDefault();
+            scroll.scrollIntoView({ behavior: "smooth" })
+             });    
+         };     
+              
+createNav();
+
+// looping through all the sections
+// create a new list element each run through the loop called 'tabs'
+// set the innerHTML of that element equal to the data attribute of the sections HTMLCollection. Double check the value there! You just want the individual item not the whole sections collection
+// append the li 'tabs' to the nav
+
+    const tabLinks = new Array();
+    const contentDivs = new Array();
+
+    function init() {
+        // Grab the tab links and content divs from the page
+        var tabListItems = document.getElementById('tabs').childNodes;
+        for ( var i = 0; i < tabListItems.length; i++ ) {
+          if (tabListItems[i].nodeName == "li" ) {
+            var tabLink = getFirstChildWithTagName(tabListItems[i], 'a');
+            var id = getHash(tabLink.getAttribute('href') );
+            tabLinks[id] = tabLink;
+            contentDivs[id] = document.getElementById(id);
+          }
+        }
+    };
+
+    
+// Add class 'active' to section when near top of viewport
+
+    function setActive(){
+        for(tabLink of contentDivs){
+            if(navchecksize(tabLink)){
+                // use the same technique to add the class to the navbar item
+                tabLink.classList.add("active__class");
+            }
+            else{        
+                tabLink.classList.remove("active__class");
+            }
+        }
+    };
+
+
+// // Go to Top Function - WIP
+// function createToTopButton() {
+//     //create new buttom element
+//     const topButton = document.createElement('button');
+
+//     // add attributes to the button
+//     topButton.id = 'toTopBtn';
+//     topButton.title = 'Go to top of webpage';
+//     topButton.innerText = 'Top';
+//     // add classes
+//     topButton.classList.add('btn');
+//     topButton.classList.add('btn-scrollTop');
+
+//     //add button to the webpage
+//     document.body.appendChild(topButton);
+// }
+
+
+/** End Main Functions * Begin Events **/
+
+
+// Make sections colaspable - WIP
+// const col = document.getElementsByTagName("collapsible");
+// var i;
+// for (i = 0; i < col.length; i++) {
+//     col[i].addEventListener("click", function() {
+//     this.classList.toggle("sectionActive");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//     content.style.display = "none";
+//     } else {
+//     content.style.display = "block";
+//     }
+//     });
+//     }
+
+// Mobile Burger Nav - WIP
+const navbarSlide = () => {
+const burger = document.querySelector('.burger');
+// const navbarLinks = document.querySelectorAll('.navbar__menu li');
+const navbarLinks = document.querySelectorAll('.navbar__list');
+
+
+burger.addEventListener('click', () => {
+    //toggle
+    nav.classList.toggle('navbar__menu active');
+    //animate
+    navbar__list.forEach((link, index) => {
+    if (link.style.animation) {
+        link.style.animation = ""
+    } else {
+        link.style.animation = 'navbar__listFade 0.7s ease forwards ${index/7}s';
+    }
+    });
+});
+}
+navbarSlide();
